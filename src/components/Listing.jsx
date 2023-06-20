fec98580521ad0379b180d8c20042d21f23a2337
Dima Shirkov <dimashirkov@list.ru>
2023-06-20 09:10:28 +0300

    add card colors depending on the amount of the remainder

--- src/components/Listing.jsx
+++ src/components/Listing.jsx
@@ -19,8 +19,8 @@ const Listing = ({data}) => {
                         </div>
                         <div className='item-details'>
                             <p className='item-title'>{item.title.length > 50 ? item.title.slice(0, 49) + '...' : item.title}</p>
-                            <p className='item-price'>{item.currency_code === 'USD' ? '$' + item.price : (item.currency_code === 'EUR' ? '&#8364; ' + item.price : item.price)}</p>
-                            <p className='item-quantity level-medium'>{item.quantity} left</p>
+                            <p className='item-price'>{item.currency_code === 'USD' ? '$' + item.price : (item.currency_code === 'EUR' ? '€' + item.price : item.price + " " + item.currency_code)}</p>
+                            <p className={item.quantity < 11 ? 'item-quantity level-low' : (item.quantity > 20 ? 'item-quantity level-high' : 'item-quantity level-medium')}>{item.quantity} left</p>
                         </div>
                     </div> 
                 )
